b42e3abefd3e829dbcf41f1b3660a65c463e2b72
Jeanluca-Moren0 <Jeanlucam12@gmail.com>
2020-09-03 15:14:35 -0300

    Initialized project and created GlobalStyles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -36,6 +36,6 @@ export default createGlobalStyle`
     --reputation-4: #F3FEE0;
     --reputation-5: #00A650;
 
-    --panel-shadow: 0 1px 3px 0 rgba(0,0,0,.3);
+    --panel-shadow: 0 1px 3px 0 rgba(0,0,0,.4);
   }
 `;
